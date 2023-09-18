e5e30af69d83d8b996cf573298f2af0c86533049
Anton Ždanov <anton@azdanov.dev>
2023-09-18 15:29:34 +0300

    add efcore, product entity and dbcontext

---
+++ API/Data/StoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreDbContext : DbContext
+{
+    public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,13 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
