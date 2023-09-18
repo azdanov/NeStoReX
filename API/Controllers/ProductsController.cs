7d68cd6c39f320d98d7feb13666c03e92fab4fd5
Anton Ždanov <anton@azdanov.dev>
2023-09-18 19:41:00 +0300

    add product migrations, seed and controller

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreDbContext _context;
+
+    public ProductsController(StoreDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await _context.Products.ToListAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+        return Ok(product);
+    }
+}
--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -5,9 +5,9 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public decimal Price { get; set; }
+    public long Price { get; set; }
     public string PictureUrl { get; set; } = string.Empty;
-    public string ProductType { get; set; } = default!;
-    public string ProductBrand { get; set; } = default!;
+    public string Type { get; set; } = default!;
+    public string Brand { get; set; } = default!;
     public int QuantityInStock { get; set; }
 }
